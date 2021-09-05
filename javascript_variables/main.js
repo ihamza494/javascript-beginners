a669d8186b1af533fab0c8a100f008a5308c1834
unknown <ihamza494@gmail.com>
2021-09-05 20:08:16 +0500

    Initial Commit

---
+++ javascript_variables/main.js
@@ -0,0 +1,15 @@
+const UserName= prompt("Enter User Name : ");
+const Password = prompt("Enter Your Password:");
+
+if (UserName == "ihamza494" &&  Password == "FASTflex2018")
+{
+  console.log("Welcome to Flex.")
+
+}
+else{
+    console.log("Incorrect credentials.")
+}
+
+//var sum = num+num2;
+
+//console.log(sum)
